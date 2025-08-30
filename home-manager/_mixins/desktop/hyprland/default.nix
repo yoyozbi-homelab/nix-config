737cb6bd598b3eb709dff841b4939a476fc8f234
yoyozbi <46626633+yoyozbi@users.noreply.github.com>
2025-08-30 17:28:04 +0200

    Switch shikane and ghostty config

--- home-manager/_mixins/desktop/hyprland/default.nix
+++ home-manager/_mixins/desktop/hyprland/default.nix
@@ -2,6 +2,8 @@
 {
   imports = [
     ./waybar.nix
+    ./ghostty.nix
+    ./shikane.nix
   ];
 
   xdg = {
@@ -23,20 +25,11 @@
       "${config.xdg.configHome}/hypr" = {
         source = ../../dotfiles/hypr;
       };
-      "${config.xdg.configHome}/shikane" = {
-        source = ../../dotfiles/shikane;
-      };
-      # "${config.xdg.configHome}/waybar" = {
-      #   source = ../dotfiles/waybar;
-      # };
       "${config.xdg.configHome}/wlogout" = {
         source = ../../dotfiles/wlogout;
       };
       "${config.xdg.configHome}/wofi" = {
         source = ../../dotfiles/wofi;
-      };
-      "${config.xdg.configHome}/ghostty" = {
-        source = ../../dotfiles/ghostty;
       };
       "${config.home.homeDirectory}/.icons/Nordzy-hyprcursors" = {
         source = ../../cursor-theme/Nordzy-hyprcursors;
---
+++ home-manager/_mixins/desktop/hyprland/shikane.nix
@@ -0,0 +1,49 @@
+{...}: {
+  programs.shikane = {
+    enable = true;
+    settings = {
+      profile = [
+        {
+          name = "docked";
+          output = [
+            {
+              search = "s=L7LMDW002550";
+              exec = ["echo This is output $SHIKANE_OUTPUT_NAME"];
+              enable = true;
+              mode = { width = 2560; height = 1440; refresh = 144; };
+              position = { x = 1080; y = 0; };
+            }
+            {
+              search = "s=KBEH41A001124";
+              exec = ["echo This is output $SHIKANE_OUTPUT_NAME"];
+              enable = true;
+              mode = { width = 1920; height = 1080; refresh = 60; };
+              position = { x = 0; y = 0; };
+              transform = "270";
+            }
+            {
+              search = "n=eDP-1";
+              exec = ["echo This is output $SHIKANE_OUTPUT_NAME"];
+              enable = true;
+              mode = { width = 1920; height = 1200; refresh = 60; };
+              position = { x = 1080; y = 1440; };
+              scale = 1;
+              }
+          ];
+        }
+        {
+          name = "builtin monitor only";
+          output = [
+            {
+              search ="n=eDP-1";
+              enable = true;
+              mode = { width = 1920; height = 1200; refresh = 60; };
+              position = { x = 0; y = 0; };
+              scale = 1.2;
+            }
+          ];
+        }
+      ];
+    };
+  };
+}
