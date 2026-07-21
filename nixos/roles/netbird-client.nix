35812e7a0d3610d49c0e62a425878bd9827e34e6
yoyozbi <46626633+yoyozbi@users.noreply.github.com>
2026-07-21 17:53:17 +0200

    fix: missing port in netbird-client role

--- nixos/roles/netbird-client.nix
+++ nixos/roles/netbird-client.nix
@@ -7,6 +7,7 @@
   services.netbird.clients.default = {
     login.enable = true;
     login.setupKeyFile = config.sops.secrets.netbird-setup-key.path;
+    port = 51820;
     login.systemdDependencies = [ "sops-install-secrets.service" ];
     config.ManagementURL = "https://netbird.yohanzbinden.ch";
   };
